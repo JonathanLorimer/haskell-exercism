25c0310f1ee459d51e03f45b9e15f10b49c19986
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-07-07 21:08:17 -0400

    completed run length

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,25 @@
 module RunLength (decode, encode) where
 
-decode :: String -> String
-decode encodedText = error "You need to implement this function."
+import Data.Char (isDigit)
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode str = case foldr go (0, []) str of
+                (0, encodedStr) -> encodedStr
+                (count, xs) -> (show (count + 1)) ++ xs
+            where
+                go cur (0, []) = (0, [cur])
+                go cur (count, acc@(prev:xs))
+                    | cur == prev   = (count + 1, acc)
+                    | count > 0     = (0, cur : ((show (count + 1)) ++ acc))
+                    | otherwise     = (0, cur : acc)
+
+decode :: String -> String
+decode str = case foldr go ("", []) str of
+                ("", decodedStr) -> decodedStr
+                (count, (x:xs)) -> (replicate (read count) x) ++ xs
+            where
+                go cur ("", []) = ("", [cur])
+                go cur (count, acc@(prev:xs))
+                    | isDigit cur   = (cur:count, acc)
+                    | count /= ""   = ("", cur : ((replicate (read count) prev) ++ xs))
+                    | otherwise     = ("", cur : acc)
