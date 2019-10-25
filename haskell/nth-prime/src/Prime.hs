0bd5280aae0316ea827fd82a824acb2d452bf941
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-10-24 22:44:47 -0400

    nth-prime

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,4 +1,8 @@
 module Prime (nth) where
 
 nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+nth n
+  | n <= 0    = Nothing
+  | otherwise = Just $ sieve [2..] !! (n - 1)
+      where sieve (p:xs) =
+             p : sieve [x | x <- xs, x `mod` p /= 0]
