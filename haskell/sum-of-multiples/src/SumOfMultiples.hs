6db5d604618a4fa77b60b7a4b1d763496060377f
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-12-03 09:05:15 -0500

    improved sum of multiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,19 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
 import Data.List
-import Data.Foldable
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples fact lim = foldl' (\s -> maybe s (s +)) 0
-                   $ fmap asum
-                   $ do
-                      a <- [0..(lim - 1)]
-                      pure $ do
-                        f <- multOf <$> fact
-                        pure (f a)
+sumOfMultiples fact lim = sum . nub . takeWhile (< lim) . sort $ mults
+  where mults = (*) <$> [1..lim] <*> fact
 
-multOf :: Integer -> Integer -> Maybe Integer
-multOf f m
-  | m == 0 || f == 0 = Nothing
-  | otherwise        = if m `mod` f == 0 then Just m else Nothing
 
