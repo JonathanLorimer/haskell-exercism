2af5e835975fecf431037a32d1e815d9f9064f60
Jonathan Lorimer <jonathan_lorimer@mac.com>
2020-02-23 16:52:47 -0500

    improvement on grains

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,11 +1,12 @@
 module Grains (square, total) where
 
+powerTwo :: Int -> Integer
+powerTwo n = 2 ^ (n - 1)
+
 square :: Int -> Maybe Integer
 square n
-  | n > 0 && n < 65 = Just $ 2^(n - 1)
+  | n > 0 && n < 65 = Just $ powerTwo n
   | otherwise = Nothing
 
 total :: Integer
-total = case traverse square [1..64] of
-          Just x  -> sum x
-          Nothing -> error "should never hit this case"
+total = sum $ powerTwo <$> [1..64]
