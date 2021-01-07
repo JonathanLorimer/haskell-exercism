ad7b0c36e2072d70716e9c4bddd3c9aa7e758b22
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-01-06 19:11:16 -0500

    remove clojure

--- clojure/beer-song/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject beer-song "0.1.0-SNAPSHOT"
-  :description "beer-song exercise."
-  :url "https://github.com/exercism/clojure/tree/master/exercises/beer-song"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.incubator "0.1.2"]])
